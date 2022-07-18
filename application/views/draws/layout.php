5ae994dd8e30b9a31b35600a5621cab01b2f1740
Ogochukwu Osuji <Ogochukwu Osuji>
2022-07-18 09:37:01 +0100

    changing Convention 2019 to Cnvention 2022

--- application/views/draws/layout.php
+++ application/views/draws/layout.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="<?=base_url('assets/css/bootstrap.min.css') ?>">
-  <title>Convention @ 2019 - Draws</title>
+  <title>Convention @ 2022 - Draws</title>
   <script type="text/javascript">
     const SITE_URL = '<?=site_url() ?>';
   </script>
@@ -14,7 +14,7 @@
 <body>
 
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Neolife Convention 2019 - Draws</a>
+    <a class="navbar-brand" href="#">Neolife Convention 2022 - Draws</a>
 
     <div class="collapse navbar-collapse">
 
--- application/views/index.php
+++ application/views/index.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="<?=base_url('assets/css/bootstrap.min.css') ?>">
-  <title>Convention @ 2019</title>
+  <title>Convention @ 2022</title>
   <script type="text/javascript">
     const SITE_URL = '<?=site_url() ?>';
   </script>
@@ -34,7 +34,7 @@
 <body>
 
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="<?=site_url() ?>">Convention 2019</a>
+    <a class="navbar-brand" href="<?=site_url() ?>">Convention 2022</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
